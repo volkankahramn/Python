2a31e0c645a287b461d9b8db360854237da630fe
volkankahramn <v07kahraman@gmail.com>
2022-11-17 09:35:03 +0300

    main.py

---
+++ main.py
@@ -0,0 +1,55 @@
+print("Merhaba Etiya")
+# string = metinsel ifade
+text = "10"
+print(text)
+
+# integer = tam sayı
+number = 10
+print(number)
+
+# double,float,decimal = ondalıklı sayı
+dnumber = 5.3
+print(dnumber)
+# boolean
+isverified = True
+print(isverified)
+
+#! matematiksel operatörler
+print(number + 5)
+print(number - 5)
+print(number * 5)
+print(number / 5)
+print(number % 3)
+#! mantıksal operatörler
+print(1 == 2)
+print(1 != 2)
+print(1 > 2)
+print(1 < 2)
+print(1 >= 2)
+print(1 <= 2)
+print(10%2 == 0)
+print(50/2 == 25)
+print(50/2 == 25.0)
+
+text = "Merhaba Etiya"
+print(text.upper())
+print(text.lower())
+print(text.startswith("M"))
+print(text.endswith("Etiya"))
+
+name = "volkan"
+age = "27"
+company = "Etiya"
+
+print(name + " " +age + " yasinda" + " " + company + "'de calisiyor")
+print(f"{name} {age} yasinda {company}'de calisiyor")
+
+
+
+
+
+
+
+
+
+
---
+++ odev.py
@@ -0,0 +1,23 @@
+baslik = "HABERİNİZ OLSUN" #string
+vade = 12 #integer
+faizOrani = 1.47 #float
+faizOrani1 = 1.43 #float
+print(baslik)
+
+#Veri tipleri hakkında bilgi almak için
+print(type(baslik))
+print(type(vade))
+print(type(faizOrani1))
+
+mesaj = "Hoşgeldin"
+musteriAdi = "Volkan"
+musteriSoyadi = "Kahraman"
+sonucMesaj = mesaj+" "+musteriAdi+" "+musteriSoyadi
+print(mesaj+" "+musteriAdi+" "+musteriSoyadi)
+print(sonucMesaj)
+
+sayi1 = 10
+sayi2 = 20
+toplamSayi = sayi1+sayi2
+print(sayi1+sayi2)
+print(toplamSayi)
